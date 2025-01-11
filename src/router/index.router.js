6c3efd5502729490b1e5fbb16afac019095c543e
victoriashopify <edugpucheta@gmail.com>
2025-01-11 19:40:24 -0300

    Add sessions router and implement current user endpoint

--- src/router/index.router.js
+++ src/router/index.router.js
@@ -2,6 +2,7 @@ import productsRouter from "./product.router.js"
 import cartsRouter from "./cart.router.js"
 import userRouter from "./user.router.js"
 import authRouter from "./auth.router.js"
+import sessionsRouter from "./sessions.router.js"
 import { Router } from "express"
 
 const router = Router();
@@ -10,6 +11,7 @@ router.use("/products", productsRouter);
 router.use("/carts", cartsRouter);
 router.use("/users", userRouter);
 router.use("/auth", authRouter);
+router.use("/sessions", sessionsRouter);
 
 
 export default router;
---
+++ src/router/sessions.router.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { verifyToken } from "../utils/jwt.utils.js";
+
+const router = Router();
+
+router.get("/current", async (req, res) => {
+  if (!req.cookies.token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const UserFromToken = verifyToken(req.cookies.token);
+  res.json({
+    message: "Current user",
+    token: req.cookies.token,
+    user: UserFromToken,
+  });
+});
+
+export default router;
