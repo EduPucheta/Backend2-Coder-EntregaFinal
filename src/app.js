30f6ebe9f7cc7a803c66f97a0b669ad9765288e9
victoriashopify <edugpucheta@gmail.com>
2025-02-11 17:15:19 -0300

    Remove placeholder test script and clean up app.js formatting

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { connectMongoDB } from "./config/mongoDb.config.js";
 import envs from "./config/envs.config.js";
-import router from "./router/index.router.js"
+import router from "./router/index.router.js";
 import passport from "passport";
 import { initializePassport } from "./config/passport.config.js";
 import cookieParser from "cookie-parser";
@@ -17,13 +17,10 @@ app.use(cookieParser());
 initializePassport();
 app.use(passport.initialize());
 
+app.use("/api", router);
 
-
-app.use("/api", router); 
- 
-const PORT = envs.PORT || 8000;   
+const PORT = envs.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${envs.PORT}`);  
-});     
- 
+  console.log(`Servidor escuchando en el puerto ${envs.PORT}`);
+});
